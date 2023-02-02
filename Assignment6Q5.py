716aff86e692bde2f7e2e30c839a344ddbd02924
gaurav22103028 <120629467+gaurav22103028@users.noreply.github.com>
2023-02-02 12:39:30 +0530

    Update Assignment6Q5.py

--- Assignment6Q5.py
+++ Assignment6Q5.py
@@ -1,6 +1,8 @@
+def funct(lis):
+    lis.sort()
+    l = len(lis)
+    for i in range(l - 1):
+        print(lis[i], end="-")
+    print(lis[l - 1])
 lis=list(map(str,input("Enter the string:").split("-")))
-lis.sort()
-l=len(lis)
-for i in range(l-1):
-    print(lis[i],end="-")
-print(lis[l-1])
+funct(lis)
