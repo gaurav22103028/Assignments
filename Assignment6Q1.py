b531cf76af2710d6f75ad32edd9bf4268670e25e
gaurav22103028 <120629467+gaurav22103028@users.noreply.github.com>
2023-02-02 12:17:04 +0530

    Update Assignment6Q1.py

--- Assignment6Q1.py
+++ Assignment6Q1.py
@@ -1,16 +1,17 @@
-n = int(input("Enter the number:"))
-sum=0
-# for i in range(1,n):
-#     if(n%i==0):
-#         sum=sum+i
-# if(n==sum):
-#     print("It is a perfect number.")
-# else:
-#     print("It is not a perfect number.")
-for i in range(1,n+1):
-    if(n%i==0):
-        sum=sum+i
-if(n==(sum/2)):
-    print("It is a perfect number.")
+def isPerfect(num):
+    sumofNum = 0###A variable to store the sum of all positive divisors
+    for i in range(1,num):
+        if num%i == 0:
+           sumofNum += i
+    if sumofNum == num:
+        return True
+    else:
+        return False
+num = int(input("Enter a positive integer:"))
+if num <= 0:
+    print("invalid input... not supported")
+    exit()
+if isPerfect(num):
+    print("Number is Perfect")
 else:
-    print("It is not a perfect number.")
+    print("Number is not Perfect")
