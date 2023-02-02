14da52a27f7a5723efb08a470dfbd68a039395bc
gaurav22103028 <120629467+gaurav22103028@users.noreply.github.com>
2023-02-02 12:29:05 +0530

    Update Assignment6Q4.py

--- Assignment6Q4.py
+++ Assignment6Q4.py
@@ -1,10 +1,14 @@
-string=str(input("Enter the string:"))
-string=string.lower()
-count=0
-for i in range(26):
-    if (chr(97+i) in string):
-        count=count+1
-if count==26:
+def Is_Pangram(string):
+    count = 0
+    for i in range(26):
+        if (chr(97 + i) in string):
+            count = count + 1
+    return(count)
+string = str(input("Enter the string:"))
+string = string.lower()
+string = string.replace(" ","")
+count=Is_Pangram(string)
+if count == 26:
     print("Entered string is a pangram.")
 else:
     print("Entered string is not a pangram.")
